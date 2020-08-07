d3bdcb7377cf33acf7dd83ee98f798d14a943519
Muthuselvam Paravai KPR <muthu.ramadoss@gmail.com>
2020-08-07 18:01:02 +0530

    java to kotlin.

--- src/main/intellibitz/sted/widgets/SplashWindow.kt
+++ src/main/intellibitz/sted/widgets/SplashWindow.kt
@@ -1,31 +1,29 @@
-package sted.widgets;
+package sted.widgets
 
-import sted.event.IStatusListener;
-import sted.event.StatusEvent;
+import sted.event.IStatusListener
+import sted.event.StatusEvent
+import java.awt.Component
+import javax.swing.BoxLayout
+import javax.swing.JProgressBar
+import javax.swing.JWindow
 
-import javax.swing.*;
-import java.awt.*;
-
-public class SplashWindow
-        extends JWindow
-        implements IStatusListener {
-    private final JProgressBar progress;
-
-    public SplashWindow(Component component) {
-        final Container contentPane = getContentPane();
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        contentPane.add(component);
-        progress = new JProgressBar(0, 100);
-        progress.setStringPainted(true);
-        contentPane.add(progress);
-        pack();
+class SplashWindow(component: Component?) : JWindow(), IStatusListener {
+    private val progress: JProgressBar
+    fun setProgress(percent: Int) {
+        progress.value = percent
     }
 
-    public void setProgress(int percent) {
-        progress.setValue(percent);
+    override fun statusPosted(event: StatusEvent) {
+        setProgress(event.status.toInt())
     }
 
-    public void statusPosted(StatusEvent event) {
-        setProgress(Integer.valueOf(event.getStatus()));
+    init {
+        val contentPane = contentPane
+        contentPane.layout = BoxLayout(contentPane, BoxLayout.Y_AXIS)
+        contentPane.add(component)
+        progress = JProgressBar(0, 100)
+        progress.isStringPainted = true
+        contentPane.add(progress)
+        pack()
     }
 }
