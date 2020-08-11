b406de5a02113099a5f94d914958d192c21d3f0a
Muthuselvam Paravai KPR <muthu.ramadoss@gmail.com>
2020-08-11 16:42:29 +0530

    java to kotlin.

--- src/main/intellibitz/sted/ui/FileViewer.kt
+++ src/main/intellibitz/sted/ui/FileViewer.kt
@@ -1,88 +1,77 @@
-package sted.ui;
+package sted.ui
 
-import sted.event.IThreadListener;
-import sted.event.ThreadEvent;
-import sted.io.FileReaderThread;
-import sted.io.Resources;
+import sted.event.IThreadListener
+import sted.event.ThreadEvent
+import sted.io.FileReaderThread
+import java.awt.Font
+import java.io.File
+import javax.swing.*
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.File;
+class FileViewer : JInternalFrame(), IThreadListener {
+    private val editorPane: JEditorPane? = JEditorPane()
+    private val fileReaderThread = FileReaderThread()
+    private var file = File("")
 
-public class FileViewer
-        extends JInternalFrame
-        implements IThreadListener {
-    private JEditorPane editorPane;
-    private File file;
-    private FileReaderThread fileReaderThread;
-
-    public FileViewer() {
-        super(Resources.EMPTY_STRING, false, false, false, false);
-        init();
-    }
-
+    /*
     public FileViewer(Icon icon) {
         this();
         setFrameIcon(icon);
     }
-
-
-    public void init() {
-        setBorder(BorderFactory.createEtchedBorder());
-        editorPane = new JEditorPane();
-        editorPane.setEditable(false);
-        final JScrollPane scroller = new JScrollPane();
-        scroller.getViewport().add(editorPane);
-        final Container contentPane = getContentPane();
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
-        contentPane.add(scroller);
-        fileReaderThread = new FileReaderThread();
-        fileReaderThread.addThreadListener(this);
-        setDefaultCloseOperation(JInternalFrame.DO_NOTHING_ON_CLOSE);
+*/
+    fun init() {
+        setTitle("")
+        isResizable = false
+        isClosable = false
+        isMaximizable = false
+        isIconifiable = false
+        border = BorderFactory.createEtchedBorder()
+        editorPane!!.isEditable = false
+        val scroller = JScrollPane()
+        scroller.viewport.add(editorPane)
+        val contentPane = contentPane
+        contentPane.layout = BoxLayout(contentPane, BoxLayout.Y_AXIS)
+        contentPane.add(scroller)
+        fileReaderThread.addThreadListener(this)
+        defaultCloseOperation = DO_NOTHING_ON_CLOSE
     }
 
     /**
      * Sets the font for this component.
      *
-     * @param font the desired <code>Font</code> for this component
-     * @see java.awt.Component#getFont
-     * <p/>
+     * @param font the desired `Font` for this component
+     * @see java.awt.Component.getFont
+     *
+     *
      * preferred: true bound: true attribute: visualUpdate true
      * description: The font for the component.
      */
-    public void setFont(Font font) {
+    override fun setFont(font: Font) {
         super.setFont(
-                font);    //To change body of overriden methods use Options | File Templates.
+            font
+        ) //To change body of overriden methods use Options | File Templates.
         if (editorPane != null) {
-            editorPane.setFont(font);
+            editorPane.font = font
         }
     }
 
-    public void setFileName(String fileName) {
-        setTitle(fileName);
-        file = new File(fileName);
+    fun setFileName(fileName: String) {
+        setTitle(fileName)
+        file = File(fileName)
     }
 
-    public void addThreadListener(IThreadListener threadListener) {
-        fileReaderThread.addThreadListener(threadListener);
+    fun addThreadListener(threadListener: IThreadListener?) {
+        fileReaderThread.addThreadListener(threadListener!!)
     }
 
-    public void readFile() {
-        fileReaderThread.setFile(file);
-        SwingUtilities.invokeLater(fileReaderThread);
+    fun readFile() {
+        fileReaderThread.file = file
+        SwingUtilities.invokeLater(fileReaderThread)
     }
 
-    public void threadRunStarted(ThreadEvent e) {
+    override fun threadRunStarted(threadEvent: ThreadEvent) {}
+    override fun threadRunning(threadEvent: ThreadEvent) {}
+    override fun threadRunFailed(threadEvent: ThreadEvent) {}
+    override fun threadRunFinished(threadEvent: ThreadEvent) {
+        editorPane!!.text = threadEvent.eventSource.result.toString()
     }
-
-    public void threadRunning(ThreadEvent e) {
-    }
-
-    public void threadRunFailed(ThreadEvent e) {
-    }
-
-    public void threadRunFinished(ThreadEvent e) {
-        editorPane.setText(e.getEventSource().getResult().toString());
-    }
-
 }
